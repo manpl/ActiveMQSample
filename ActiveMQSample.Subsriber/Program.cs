461327f2d07b570cef8702eb4fa0508b835917dc
nawrockim <nawrockim@op.pl>
2015-12-31 00:04:29 +0100

    copy

---
+++ ActiveMQSample.Subsriber/Program.cs
@@ -0,0 +1,42 @@
+﻿using Apache.NMS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveMQSample.Subsriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri connecturi = new Uri("activemq:tcp://localhost:61616");
+
+            Console.WriteLine("About to connect to " + connecturi);
+
+            IConnectionFactory factory = new NMSConnectionFactory(connecturi);
+
+            using (IConnection connection = factory.CreateConnection())
+            using (ISession session = connection.CreateSession())
+            {
+                IDestination destination = session.GetDestination("queue://FOO.BAR");
+
+                connection.Start();
+                Console.WriteLine("Connection established");
+
+                using (var consumer = session.CreateConsumer(destination))
+                {
+                    while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape))
+                    {
+                        var message = (ITextMessage)consumer.Receive();
+                        Console.WriteLine(message.Text);
+                        Console.WriteLine("------------------------------------------");
+                    }
+                }
+            }
+
+            Console.WriteLine("Terminating..");
+        }
+    }
+}
---
+++ ActiveMQSample/Program.cs
@@ -0,0 +1,41 @@
+﻿using Apache.NMS;
+using System;
+
+namespace ActiveMQSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri connecturi = new Uri("activemq:tcp://localhost:61616");
+
+            Console.WriteLine("About to connect to " + connecturi);
+
+            IConnectionFactory factory = new NMSConnectionFactory(connecturi);
+
+            using (IConnection connection = factory.CreateConnection())
+            using (ISession session = connection.CreateSession())
+            {
+                IDestination destination = session.GetDestination("queue://FOO.BAR");
+                connection.Start();
+                Console.WriteLine("Connection established");
+
+                using (IMessageProducer producer = session.CreateProducer(destination))
+                {
+                    producer.DeliveryMode = MsgDeliveryMode.NonPersistent;
+                    Console.WriteLine("Press enter to send another message, q to quit");
+                    while((Console.ReadLine()??"").ToLower() != "q")
+                    {
+                        var payload = "Hello there. " + DateTime.Now.ToLongTimeString();
+                        var message = session.CreateTextMessage(payload);
+                        producer.Send(message);
+                        Console.WriteLine("Message sent. " + payload);
+                        Console.WriteLine("------------------------------------------");
+                    }
+                }
+            }
+
+            Console.WriteLine("Terminating..");
+        }
+    }
+}
